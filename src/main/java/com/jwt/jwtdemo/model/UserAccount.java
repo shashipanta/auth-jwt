d158022c9f494c18d740135013e266bd50267d0a
shashipanta <shashipanta57@gmail.com>
2023-06-01 15:27:52 +0545

    Initial:Created entity class for UserAccount

--- src/main/java/com/jwt/jwtdemo/model/UserAccount.java
+++ src/main/java/com/jwt/jwtdemo/model/UserAccount.java
@@ -1,2 +1,29 @@
-package com.jwt.jwtdemo.model;public class UserAccount {
+package com.jwt.jwtdemo.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "user")
+public class UserAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name", length = 100, nullable = false)
+    private String username;
+
+    @Column(name = "email", length = 150, nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "phone_no", nullable = false)
+    private String phoneNumber;
+
+
 }
